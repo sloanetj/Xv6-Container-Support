62192fb78af1a5912bcf68b35a30c2b2746a3036
Lily Schwarz <31601251+lilyrschwarz@users.noreply.github.com>
2019-11-18 00:23:13 -0500

    testing

--- defs.h
+++ defs.h
@@ -187,6 +187,8 @@ void   switchuvm(struct proc *);
 void   switchkvm(void);
 int    copyout(pde_t *, uint, void *, uint);
 void   clearpteu(pde_t *pgdir, char *uva);
+char*  mappage(struct proc *pg);
+int    shmpg_unmap(struct shm_pg *pg);
 
 // number of elements in fixed-size array
 #define NELEM(x) (sizeof(x) / sizeof((x)[0]))
