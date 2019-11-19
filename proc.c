cc026c5bb5a481709bd2a216088a8289918a8f33
Lily Schwarz <31601251+lilyrschwarz@users.noreply.github.com>
2019-11-19 16:51:39 -0500

    finding page fault

--- proc.c
+++ proc.c
@@ -522,12 +522,16 @@ findpage(char* name)
 	int pg_num = 0;
 	for(pg = shmtable.pages; pg < &shmtable.pages[SHM_MAXNUM]; pg++)
 	{
-		cprintf("THE BARE MINIMUM lol");
 
 		if(strncmp(pg->name, name,200) == 0)
 		{
+			cprintf("THE BARE MINIMUM haha");
+
 			if(!pg->allocated)
 			{
+
+				cprintf("THE BARE MINIMUM MMMMMMMMMMMM");
+
 				//allocate page
 				pg->pa = kalloc();
 				pg->allocated = 1;
