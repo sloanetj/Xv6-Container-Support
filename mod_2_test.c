8b255213fa2d208fe56a2ee050b9e5c96aed67c6
Lily Schwarz <31601251+lilyrschwarz@users.noreply.github.com>
2019-11-18 21:35:50 -0500

    Testing

--- mod_2_test.c
+++ mod_2_test.c
@@ -13,11 +13,11 @@ main(void)
   int test = 1;
   *shared_memory_address1 = test;
 
-  if (fork() == 0){
-    char* shared_memory_address2 = shm_get("Test");
-    *shared_memory_address2 -= 1;
-    exit();
-  }
+  // if (fork() == 0){
+  //   char* shared_memory_address2 = shm_get("Test");
+  //   *shared_memory_address2 -= 1;
+  //   exit();
+  // }
   wait();
   printf(1,"%d\n", *shared_memory_address1);
   exit();
