01cbe1e5c0f18e73484a727ffd7e8081a0bc2797
Lily Schwarz <31601251+lilyrschwarz@users.noreply.github.com>
2019-11-21 18:30:16 -0500

    testing find page

--- mod_2_test.c
+++ mod_2_test.c
@@ -7,7 +7,7 @@
 int
 main(void)
 {
-
+  printf("START OF MAIN\n");
   char* shared_memory_address1 = shm_get("test");
 
   // int test = 1;
@@ -19,7 +19,8 @@ main(void)
   //   exit();
   // }
   //wait();
-  printf(1, shared_memory_address1);
 
+  printf(1, shared_memory_address1);
+  printf(1, "END OF MAIN\n");
   exit();
 }
