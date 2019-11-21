ea9e3ee1f2d19539fbfb41aadd8ce66f3f4f6e7b
Lily Schwarz <31601251+lilyrschwarz@users.noreply.github.com>
2019-11-20 19:25:33 -0500

    testing allocating new page

--- proc.c
+++ proc.c
@@ -544,9 +544,23 @@ findpage(char* name)
 			if(!pg->allocated)
 			{
 				//allocate page
+				// pg->pa = kalloc();
+				// pg->allocated = 1;
+				// memset(pg->pa, 0, 4096);
+
+				//allocate page
+				cprintf("HERE 1");
 				pg->pa = kalloc();
+				cprintf("HERE 2");
+
+				pg->name = name;
+				cprintf("HERE 3");
+
 				pg->allocated = 1;
+				cprintf("HERE 4");
+
 				memset(pg->pa, 0, 4096);
+				cprintf("HERE 5");
 			}
 			return pg_num;
 		}
@@ -577,19 +591,10 @@ shmget(char* name)
 	else
 	{
 		//allocate page
-		cprintf("HERE 1");
 		pg->pa = kalloc();
-		cprintf("HERE 2");
-
 		pg->name = name;
-		cprintf("HERE 3");
-
 		pg->allocated = 1;
-		cprintf("HERE 4");
-
 		memset(pg->pa, 0, 4096);
-		cprintf("HERE 5");
-
 	}
 
 	vas = mappage(pg);
