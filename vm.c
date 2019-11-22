84e15a4fec213a770b30a84d934643302461875c
Lily Schwarz <31601251+lilyrschwarz@users.noreply.github.com>
2019-11-21 19:31:28 -0500

    testing shm_get

--- vm.c
+++ vm.c
@@ -463,10 +463,10 @@ shmget(char* name)
 	{
 		if(pg->allocated == 0)
 		{
-			strncpy(pg->name, name, 50);
-			pg->allocated = 1;
-			pg->pa = kalloc();
-			pg->ref_count = 1;
+			// strncpy(pg->name, name, 50);
+			// pg->allocated = 1;
+			// pg->pa = kalloc();
+			// pg->ref_count = 1;
 			// memset(pg->pa, 0, PGSIZE);
 			// mappages(myproc()->pgdir, (void*)PGROUNDUP(myproc()->sz), PGSIZE, V2P(pg->pa), PTE_P | PTE_W | PTE_U);
 			// vas = (char*)PGROUNDUP(myproc()->sz);
