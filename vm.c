d83aec4fb4a66ca22c5821f0f53616f672a8260f
Lily Schwarz <31601251+lilyrschwarz@users.noreply.github.com>
2019-11-21 22:52:30 -0500

    testing shm_get

--- vm.c
+++ vm.c
@@ -472,7 +472,7 @@ shmget(char* name)
 			mappages(myproc()->pgdir, (void*)PGROUNDUP(myproc()->sz), PGSIZE, V2P(pg->pa), PTE_P | PTE_W | PTE_U);
 			vas = (char*)PGROUNDUP(myproc()->sz);
 			myproc()->sz += PGSIZE;
-			cprintf(vas);
+			cprintf("lol  %d  ", vas);
 			return name;
 		}
 	}
