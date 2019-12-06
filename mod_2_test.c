12cc3c8d672dbdf60103d6ca554051c798bdd0c5
Lily Schwarz <31601251+lilyrschwarz@users.noreply.github.com>
2019-12-06 16:17:35 -0500

    saving name

--- mod_2_test.c
+++ mod_2_test.c
@@ -24,7 +24,7 @@ main(void)
 
 
   char* shared_memory_address1 = shm_get("test1");
-
+  *shared_memory_address1 = 1;
   if(fork() == 0)
   {
     shared_memory_address1 = shm_get("test1");
@@ -40,10 +40,9 @@ main(void)
   //   // shared_memory_address1 = shm_get("test1");
   //   // printf(1, "  %d   \n", *shared_memory_address1);
   // }
-  printf(1, "  %d   \n", *shared_memory_address1);
   wait();
-
-   exit();
+  printf(1, "  %d   \n", *shared_memory_address1);
+  exit();
 }
 
 // char* shared_memory_address1 = shm_get("test1");
