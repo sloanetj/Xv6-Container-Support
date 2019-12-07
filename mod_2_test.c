44ba106eeac2b072b691c01d2133250fdd0b2a2d
Lily Schwarz <31601251+lilyrschwarz@users.noreply.github.com>
2019-12-06 20:59:15 -0500

    memory was shared

--- mod_2_test.c
+++ mod_2_test.c
@@ -39,10 +39,12 @@ main(void)
 
     shared_memory_address1 = shm_get("test1");
     printf(1, "  %d   \n", *shared_memory_address1);
+    exit();
+
   }
 
+  exit();
 
-   exit();
 }
 
 // char* shared_memory_address1 = shm_get("test1");
